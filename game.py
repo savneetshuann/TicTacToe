563a86cdad7d47477e0c02ff2e1e742db5b6ac14
cypher97 <vedchokshi1597@gmail.com>
2021-04-17 23:54:19 -0400

    commit

--- game.py
+++ game.py
@@ -2,12 +2,12 @@
 
 
 import random
-import tkinter
 from tkinter import *
 import sqlite3
 from functools import partial
 from tkinter import messagebox
 from copy import deepcopy
+from scoreboard import root
 
 # sign variable to decide the turn of which player
 sign = 0
@@ -32,6 +32,7 @@ def winner(b, l):
 
 
 # Configure text on button while playing with another player
+
 def get_value(i, j, gb, l1, l2):
     global sign
 
@@ -96,6 +97,10 @@ def get_value(i, j, gb, l1, l2):
     elif (isfull()):
         gb.destroy()
         box = messagebox.showinfo("Tie Game", "Tie Game")
+
+
+
+#def scoreboard():
 
 
 # Check if the player can push the button or not
@@ -234,12 +239,13 @@ def with_machine(game_board):
 
 
 # Initialize the game board to play with another player
-def with_player(game_board,user_name1):
+def with_player(game_board,user_name1,user_name2):
     global username1
+    global username2
     username1=user_name1.get()
-    print(username1)
-
-
+    username2=user_name2.get()
+    print(username1) #test
+    print(username2) #test
     game_board.destroy()
     game_board = Tk()
     game_board.title("Tic Tac Toe")
@@ -277,7 +283,7 @@ def open_single(game_board):
     user_name_label.grid(row=0, column=0)
     user_name = Entry(game_board, width=30)
     user_name.grid(row=1, column=0, padx=20)
-    submit_btn = Button(game_board, text="Play", command=wpc, activeforeground='white',
+    submit_btn = Button(game_board, text="Play", command=lambda : wpc(user_name), activeforeground='white',
                         activebackground="grey", bg="blue", fg="white", font='Gabriola')
     submit_btn.grid(row=2, column=0, pady=10, padx=10)
     game_board.mainloop()
@@ -291,7 +297,6 @@ def insert():
 def open_multiple():
     top = Tk()
 
-
     user_name_label1 = Label(top, text="Enter username for Player1: ")
     user_name_label1.grid(row=0, column=0)
     user_name1 = Entry(top, width=30)  # user1 from here need to insert in databse
@@ -302,7 +307,7 @@ def open_multiple():
     user_name2 = Entry(top, width=30)
     user_name2.grid(row=3, column=0, padx=20)
     wpl = partial(with_player, top)
-    submit_btn = Button(top, text="Play", command=lambda: wpl(user_name1), activeforeground='white',
+    submit_btn = Button(top, text="Play", command=lambda: wpl(user_name1,user_name2), activeforeground='white',
                         activebackground="grey", bg="blue", fg="white", font='Gabriola')
     submit_btn.grid(row=4, column=0, pady=10, padx=10)
     top.mainloop()
@@ -315,20 +320,12 @@ def run():
     menu.title("Tic Tac Toe")
 
     single = partial(open_single, menu)
-    # op=partial(open_single,menu)
-
-    # submit_btn = Button(top, "PLAY")
-    # submit_btn.grid(row=2, column=0, pady=10, padx=10)
 
     head = Label(menu, text="Welcome to tic-tac-toe",
                  activeforeground='white',
                  activebackground="black", bg="white",
                  fg="black", width=500, font='Modern', bd=5)
 
-    # menu1 = Button(menu, text="Single Player", command=wpc,
-    #                activeforeground='white',
-    #                activebackground="grey", bg="blue",
-    #                fg="white", width=500, font='Gabriola', bd=5)
     menu1 = Button(menu, text="Single Player", command=single, activeforeground='white',
                    activebackground="grey", bg="blue", fg="white",
                    width=500, font='Gabriola', bd=5)
@@ -337,15 +334,20 @@ def run():
                    activebackground="grey", bg="blue", fg="white",
                    width=500, font='Gabriola', bd=5)
 
-    menu3 = Button(menu, text="Exit", command=menu.quit, activeforeground='white',
+    menu3 = Button(menu, text="Scoreboard", command=root.mainloop(), activeforeground='white',
                    activebackground="grey", bg="blue", fg="white",
                    width=500, font='Gabriola', bd=5)
+
+    menu4 = Button(menu, text="Exit", command=menu.quit, activeforeground='white',
+                   activebackground="grey", bg="blue", fg="white",
+                   width=500, font='Gabriola', bd=5)
+
     head.pack(side='top')
     menu1.pack(side='top')
     menu2.pack(side='top')
     menu3.pack(side='top')
+    menu4.pack(side='top')
     menu.mainloop()
-
 
 # table creation
 # connection()
---
+++ scoreboard.py
@@ -0,0 +1,55 @@
+from tkinter import ttk
+
+import tkinter as tk
+
+import sqlite3
+
+
+def connect():
+    con1 = sqlite3.connect('player_info.db')
+    cur1 = con1.cursor()
+    cur1.execute("CREATE TABLE IF NOT EXISTS players(user_name text, no_of_wins integer, points integer)")
+
+    con1.commit()
+    con1.close()
+
+
+def View():
+    con1 = sqlite3.connect('player_info.db')
+    cur1 = con1.cursor()
+    cur1.execute("Select user_name,no_of_wins,points from players")
+    rows = cur1.fetchall()
+
+    for row in rows:
+        print(row)
+
+        tree.insert("", tk.END, values=row)
+
+    con1.close()
+
+
+connect()
+
+root = tk.Tk()
+
+tree = ttk.Treeview(root, column=("c1", "c2", "c3"), show='headings')
+
+tree.column("#1", anchor=tk.CENTER)
+
+tree.heading("#1", text="NAME")
+
+tree.column("#2", anchor=tk.CENTER)
+
+tree.heading("#2", text="WINS")
+
+tree.column("#3", anchor=tk.CENTER)
+
+tree.heading("#3", text="POINTS")
+
+tree.pack()
+
+button1 = tk.Button(text="Display data", command=View)
+
+button1.pack(pady=10)
+
+root.mainloop()
